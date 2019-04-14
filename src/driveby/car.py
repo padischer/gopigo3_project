317a4ac78bd797f7898cb20c8ee1839a84580ec1
padischer <sven.walser26@gmail.com>
2019-04-14 21:22:53 +0200

    the sourcecode

---
+++ src/driveby/car.py
@@ -0,0 +1,64 @@
+import easygopigo3 as easy
+import sys
+import time
+import logging
+
+class car:
+
+    ismoving = False
+    gpg = easy.EasyGoPiGo3()
+
+    def drive(self, instructions):
+        try:
+            my_linefollower = self.gpg.init_line_follower()
+            time.sleep(0.1)
+        except:
+            logging.error('Line Follower not responding')
+            time.sleep(0.2)
+            sys.exit()
+
+        logging.info("start driving")
+        self.gpg.forward()
+
+        while True:
+            logging.debug(my_linefollower.read_position())
+            if my_linefollower.read_position() == 'black':
+                logging.debug("black detected")
+                self.gpg.stop()
+                time.sleep(0.2)
+                self.gpg.drive_cm(5)
+                time.sleep(0.2)
+
+
+                logging.debug(my_linefollower.read_position())
+                if my_linefollower.read_position() in ['center', "left", "right"]:
+                    next_instruction = instructions[0]
+                    
+                    logging.debug("executing next instruction")
+                    logging.info(next_instruction)
+
+                    if next_instruction is "left":
+                       self.gpg.turn_degrees(270)
+                    if next_instruction is "right":
+                       self.gpg.turn_degrees(90)
+                       logging.debug("going forward")
+                    time.sleep(0.2)
+                    self.gpg.forward()
+                    del instructions[0]                
+
+
+                if my_linefollower.read_position() is "white":
+                    self.gpg.stop()
+                    time.sleep(0.2)
+                    self.gpg.turn_degrees(180)
+                    time.sleep(1)
+                    return True
+
+            if my_linefollower.read_position() == 'center':
+                self.gpg.forward()
+            if my_linefollower.read_position() == 'left':
+                self.gpg.left()
+                self.gpg.left()
+            if my_linefollower.read_position() == 'right':
+                self.gpg.right()
+                self.gpg.right()
